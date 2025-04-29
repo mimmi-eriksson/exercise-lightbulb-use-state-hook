f65cead6645d1300b063b49949ac1b9bbcbfe0b9
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-04-29 14:09:30 +0200

    after workshop

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react"
+
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [isOn, setIsOn] = useState(false)
+  const [isDisco, setIsDisco] = useState(false)
 
   const toggleLight = () => {
     // Toggle the on/off state
     console.log("Clicked");
+
+    if (isOn) {
+      setIsOn(false)
+      setIsDisco(false)
+    } else {
+      setIsOn(true)
+    }
   };
+
+  const toggleDisco = () => {
+    isDisco ? setIsDisco(false) : setIsDisco(true)
+  }
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+      <div className={`lightbulb ${isOn ? 'on' : 'off'} ${isDisco ? 'disco' : ''}`} onClick={toggleLight}>
         <div className="bulb"></div>
       </div>
+      <button className="disco-button" disabled={!isOn} onClick={toggleDisco} >Disco!</button>
     </div>
   );
 };
